5e8ab59185db7679f9dbbd0a07e7083887f8d457
DevCutup <takasi75926@gmail.com>
2024-10-05 23:47:12 +0200

    homepage started

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="h-screen overflow-hidden">
+      <body className="">
         <ReactQueryProvider>
           <ClusterProvider>
             <SolanaProvider>
